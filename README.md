c97d013f331bd884284b3f657844f8b1b487f2e3
Benjamin <benjamin@hellomojito.com>
2020-03-26 10:28:53 +0100

    readMe

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Laravel trainings
 ---
 This app is running with homestead
 
+Node js websocket server
+`laravel-echo-server start`
+
--- resources/assets/js/laravelEcho.js
+++ resources/assets/js/laravelEcho.js
@@ -1,20 +1,19 @@
-import Echo from "laravel-echo"
+import Echo from 'laravel-echo';
 window.io = require('socket.io-client');
 
 let e = new Echo({
-    broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001' // this is laravel-echo-server host
+	broadcaster: 'socket.io',
+	host: window.location.hostname + ':6001', // this is laravel-echo-server host
 });
 
-e.channel('test-channel')
-    .listen('UserSignedUp', (e) => {
-        console.log(e)
-        let item = document.createElement("p");
-        item.innerHTML = e.username;
-        document.querySelector('body').appendChild(item);
-    })
+e.channel('test-channel').listen('UserSignedUp', e => {
+	console.log(e);
+	let item = document.createElement('p');
+	item.innerHTML = e.username;
+	document.querySelector('body').appendChild(item);
+});
 
-document.querySelector('#notify').addEventListener('click', function(e){
-    e.preventDefault()
-    fetch('/');
-})
+document.querySelector('#notify').addEventListener('click', function(e) {
+	e.preventDefault();
+	fetch('/');
+});
