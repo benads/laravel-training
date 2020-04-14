49e9b0142d5c6c7180630997a3b23a889d063734
Benjamin <benjaminad5@yahoo.com>
2020-04-14 14:25:53 +0200

    feat(relationship) many to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $guarded = [];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 }
