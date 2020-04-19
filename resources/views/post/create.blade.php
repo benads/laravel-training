14e3cbff2edb3b6771e9ee2c03e0d003d4e596fb
Benjamin <benjaminad5@yahoo.com>
2020-04-19 23:44:58 +0200

    feat(post) bootstrap create form

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,15 +1,13 @@
 <form method="POST" action="{{ route('post.store') }}">
     {{ csrf_field() }}
-    <label class="label">Titre</label>
-    <div class="control">
-        <input class="input" type="text" name="title" placeholder="Titre du post">
+    <div class="form-group">
+        <input class="form-control" type="text" name="title" placeholder="Titre du post">
     </div>
-    <label class="label">Contenu</label>
-    <div class="control">
-        <input class="input" type="text" name="content" placeholder="Contenu du post">
+    <div class="form-group">
+        <input class="form-control" type="text" name="content" placeholder="Contenu du post">
     </div>
     <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
     <div class="control">
-        <button class="button is-link">Envoyer</button>
+        <button class="btn btn-primary">Envoyer</button>
     </div>
 </form>
