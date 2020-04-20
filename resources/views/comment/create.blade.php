40e4b50315471711e3568e02b4a3db34e1b71503
Benjamin <benjaminad5@yahoo.com>
2020-04-20 23:30:39 +0200

    fix(validation) error message unused

--- resources/views/comment/create.blade.php
+++ resources/views/comment/create.blade.php
@@ -1,12 +1,3 @@
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
 <form method="POST" action="{{ route('comment.store', ['id' => $id]) }}">
     {{ csrf_field() }}
     <div class="form-group">
