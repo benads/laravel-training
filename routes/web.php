5860ed16b59511c1ba1c045a40798fb11edaa376
Benjamin <benjaminad5@yahoo.com>
2020-04-30 17:53:08 +0200

    refacto: delete all unused routes

--- routes/web.php
+++ routes/web.php
@@ -14,28 +14,3 @@ use App\Http\Controllers\Auth\LoginController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Route::group(['middleware' => ['auth']], function () {
-//     Route::get('/', 'HomeController@index')->name('home');
-
-//     Route::get('/user/{id}', 'UserController@show')->name('user.show');
-
-//     Route::get('/post/{id}', 'PostController@show')->name('post.show');
-
-//     Route::post('/post/{id}/comment/create', 'CommentController@store')->name('comment.store');
-
-//     Route::post('/post/create', 'PostController@store')->name('post.store');
-
-//     Route::post('/post/{id}/like', 'LikeController@like')->name('like');
-
-//     Route::delete('/post/{id}/unlike', 'LikeController@unlike')->name('unlike');
-
-//     Route::get('authById/{user_id}', 'GroupController@authById')->name('authById');
-
-//     Route::post('groups/{id}/notify', 'GroupController@notify')->name('notify');
-
-//     Route::get('/groups', 'GroupController@index')->name('groups');
-// });
-
-
-// Auth::routes();
