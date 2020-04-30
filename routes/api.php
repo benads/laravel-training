20dd17c6dfca1413ae2b922e85a6ecf016b660f3
Benjamin <benjaminad5@yahoo.com>
2020-04-30 14:56:59 +0200

    feat(API) GET post

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('post', function () {
+    return response(Post::all(), 200);
 });
