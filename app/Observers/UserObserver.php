f2d79e942bb089ad1579199129ae9cce1237d67b
Benjamin <benjaminad5@yahoo.com>
2020-05-08 00:15:25 +0200

    fix(listFriend) fix : id need to be the = user_id

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -16,6 +16,7 @@ class UserObserver
     public function created(User $user)
     {
         FriendList::create([
+            "id" => $user->id,
             "user_id" => $user->id
         ]);
     }
