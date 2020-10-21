7ee3ec3d7b3d6d3b3c2c4f4f283f393fe414c73a
Benjamin <benjaminad5@yahoo.com>
2020-10-21 19:11:36 +0200

    feat(users) change response endpoint

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -13,9 +14,24 @@ class UserController extends Controller
      * @param int $id
      * @return void
      */
-    public function show()
+    public function me()
     {
         $user = Auth::user();
+        
+        return response($user, 200);
+    }
+
+    public function index(Request $request)
+    {
+        $users = $request->query() ? User::whereLike('pseudo', $request->query()['s'])->get() : User::all();
+        
+        return response($users, 200);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+
         return response($user, 200);
     }
 }
